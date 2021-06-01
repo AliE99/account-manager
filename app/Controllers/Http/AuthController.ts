bc17ad535f4ba8c09a6f2799c83033abb6e37068
Ali <alielahi.1378@gmail.com>
2021-06-01 10:32:19 +0430

    Feat : Validate AuthController data

--- app/Controllers/Http/AuthController.ts
+++ app/Controllers/Http/AuthController.ts
@@ -1,25 +1,31 @@
 import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 import User from 'App/Models/User'
-// import { rules, schema } from '@ioc:Adonis/Core/Validator'
+import { rules, schema } from '@ioc:Adonis/Core/Validator'
 
 export default class AuthController {
   public async login({ request, auth }: HttpContextContract) {
-    const email = request.input('email')
-    const password = request.input('password')
-    const token = await auth.use('api').attempt(email, password, {
+    const ValidationSchema = schema.create({
+      email: schema.string({ trim: true }, [rules.email()]),
+      password: schema.string({ trim: true }),
+    })
+    const validatedData = await request.validate({ schema: ValidationSchema })
+    const token = await auth.use('api').attempt(validatedData.email, validatedData.password, {
       expiresIn: '10 days',
     })
     return token.toJSON()
   }
+
   public async register({ request, auth }: HttpContextContract) {
-    const email = request.input('email')
-    const password = request.input('password')
-    const name = request.input('name')
-    const newUser = new User()
-    newUser.email = email
-    newUser.password = password
-    newUser.name = name
-    await newUser.save()
+    const ValidationSchema = schema.create({
+      name: schema.string({ trim: true }),
+      email: schema.string({ trim: true }, [
+        rules.email(),
+        rules.unique({ table: 'users', column: 'email' }),
+      ]),
+      password: schema.string({ trim: true }),
+    })
+    const validatedData = await request.validate({ schema: ValidationSchema })
+    const newUser = await User.create(validatedData)
     const token = await auth.use('api').login(newUser, {
       expiresIn: '10 days',
     })
